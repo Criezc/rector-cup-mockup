0b25115c91eb523374e9e7f01aa7808ca5da3b2d
Criezc <friscolieyanto@gmail.com>
2022-09-13 00:37:57 +0700

    commit v1

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        BgBlue: "#0089f7",
+        BgDark: "#021617",
+      },
+    },
+  },
+  plugins: [],
+};
